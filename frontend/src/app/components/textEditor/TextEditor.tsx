d3338cb8cbb9d320acf28d2b6ced49839980f9cc
kgolikov <kgolikov@37bytes.com>
2022-01-30 14:15:04 +0500

    A lot of changes (frontend)

--- frontend/src/app/components/textEditor/TextEditor.tsx
+++ frontend/src/app/components/textEditor/TextEditor.tsx
@@ -1,13 +1,20 @@
 import React, {useEffect, useMemo, useState} from 'react';
-import useTextEditorWebSocket from '../../hooks/api/useTextEditorWebSocket';
+import useTextEditorWebSocket, {TextEditorWebSocketParams} from '../../hooks/api/useTextEditorWebSocket';
 import getFileContent from '../actions/api/getFileContent';
+import {v4} from 'uuid';
+import styles from './TextEditor.module.scss';
+import './TextEditor.scss';
+import {Tabs} from 'antd';
+import classNames from 'classnames';
+const {TabPane} = Tabs;
 
 const TextEditor = () => {
     const [text, setText] = useState<string>();
 
-    const wsParams = useMemo(() => ({
+    const wsParams = useMemo<TextEditorWebSocketParams>(() => ({
+        sessionId: v4(),
         onFileChange: setText,
-            onUserSelectionChange: () => {
+        onUserSelectionChange: () => {
             console.log('onUserSelectionChange')
         }
     }), [])
@@ -16,7 +23,7 @@ const TextEditor = () => {
 
     const onTextChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
         const newText = event.target.value;
-        // setText(newText);
+        setText(newText);
         changeFile(newText);
     };
 
@@ -25,13 +32,19 @@ const TextEditor = () => {
             getFileContent().then(content => {
                 setText(content);
             });
-            return;
         }
     }, [text]);
 
     return (
-        <div>
-            <textarea value={text} onChange={onTextChange} readOnly={text === undefined}/>
+        <div className={classNames(styles.container, 'TextEditor')}>
+            <Tabs defaultActiveKey="1" className={styles.fullSize}>
+                <TabPane tab="Raw" key="1" className={classNames(styles.fullSize, styles.rawTab)}>
+                    <textarea className={styles.textarea} value={text} onChange={onTextChange} readOnly={text === undefined}/>
+                </TabPane>
+                <TabPane tab="HTML" key="2" className={classNames(styles.fullSize, styles.htmlTab)}>
+                    <div dangerouslySetInnerHTML={{__html: text ?? ''}}/>
+                </TabPane>
+            </Tabs>
         </div>
     );
 };
--- frontend/src/app/hooks/api/useTextEditorWebSocket.ts
+++ frontend/src/app/hooks/api/useTextEditorWebSocket.ts
@@ -2,50 +2,68 @@ import SockJS from 'sockjs-client';
 import Stomp from 'stompjs';
 import {BACKEND_HOST} from '../../constants/env';
 import {useCallback, useEffect, useRef} from 'react';
-import {v4} from 'uuid';
 
-const createClient = () => {
-    const sessionId = v4();
+let client: {
+    client: Stomp.Client,
+    sessionId: string
+};
+
+const createClient = (sessionId: string) => {
+    if (client) {
+        return client;
+    }
+
     console.log('sessionId', sessionId);
     const socket = new SockJS(BACKEND_HOST + '/websocket', undefined, {
         sessionId: () => sessionId
     });
-    return {
+    client = {
         client: Stomp.over(socket),
         sessionId
     };
+    return client;
+};
+
+const getFrameSessionId = (frame: Stomp.Frame) => {
+    const headers = frame.headers as Record<string, string>;
+    const messageId = headers['message-id'];
+    return /(.+)-\d+$/.exec(messageId)?.[1];
 };
 
 export interface TextEditorWebSocketParams {
+    sessionId: string;
     onFileChange: (content: string) => void;
     onUserSelectionChange: (userSelection: object) => void;
 }
 
-const useTextEditorWebSocket = ({onFileChange, onUserSelectionChange}: TextEditorWebSocketParams) => {
-    const {client, sessionId} = useRef(createClient()).current;
+const useTextEditorWebSocket = ({sessionId, onFileChange, onUserSelectionChange}: TextEditorWebSocketParams) => {
+    const {client} = useRef(createClient(sessionId)).current;
 
     const changeFile = useCallback((fileContent: string) => {
         if (!client.connected) {
             return;
         }
 
-        client.send('/app/fileEditor/fileChange', {}, JSON.stringify(fileContent));
-    }, [client]);
+        client.send('/app/fileEditor/fileChange', {sessionId}, JSON.stringify(fileContent));
+    }, [client, sessionId]);
 
     const changeSelection = useCallback((selection: object) => {
         if (!client.connected) {
             return;
         }
 
-        client.send('/app/fileEditor/selectionChange', {}, JSON.stringify(selection));
-    }, [client]);
+        client.send('/app/fileEditor/selectionChange', {sessionId}, JSON.stringify(selection));
+    }, [client, sessionId]);
 
     useEffect(() => {
         client.connect({}, () => {
             console.log('WS: Connected');
 
             client.subscribe('/fileEditor/changes/file', (message) => {
-                console.log('subscribe file', message)
+                if (getFrameSessionId(message) === sessionId) {
+                    return;
+                }
+
                 onFileChange(JSON.parse(message.body));
             });
 
